5c7f9f73b2a7a6e70398da9b0b61a9f043c82ffd
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-04-25 00:41:34 +0700

    finish styling using tailwind css

--- cilent/src/features/order/OrderItem.tsx
+++ cilent/src/features/order/OrderItem.tsx
@@ -8,12 +8,12 @@ function OrderItem({ item, isLoadingIngredients, ingredients }: OrderItemProp) {
     const { quantity, name, totalPrice } = item;
   
     return (
-      <li>
-        <div>
+      <li className="py-3">
+        <div className="flex items-center justify-between gap-4 text-sm">
           <p>
-            <span>{quantity}&times;</span> {name}
+            <span className="font-bold">{quantity}&times;</span> {name}
           </p>
-          <p>{formatCurrency(totalPrice)}</p>
+          <p className="font-bold">{formatCurrency(totalPrice)}</p>
         </div>
       </li>
     );
--- cilent/src/ui/Button.tsx
+++ cilent/src/ui/Button.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Button({ children, disabled, to, type }) {
-    const base = "inline-block bg-yellow-400 uppercase tracking-wide font-semibold text-stone-800 rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+    const base = "inline-block text-sm bg-yellow-400 uppercase tracking-wide font-semibold text-stone-800 rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
     const styles = {
         primary: base + 'px-4 py-3 sm:px-6 sm:py-4',
-        small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs'
+        small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        secondary: "inline-block text-sm bg-transparent border-2 border-stone-300 uppercase tracking-wide font-semibold text-stone-800 rounded-full hover:bg-stone-300 transition-colors text-stone-400 duration-300 focus:outline-none focus:ring focus:ring-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:ring-offset-2 focus:text-stone-800 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6 sm:py-3.5"
     }
     
     if (to)
